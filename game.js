571892e101ac7687fb130ab929c03e8c6745aef0
TJ Hackett <thackett117@gmail.com>
2023-04-01 09:51:11 -0500

    added hud with score and number of questions

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question'); 
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -53,6 +55,8 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -82,6 +86,10 @@ choices.forEach(choice => {
 
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+        if(classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        };
+
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
@@ -92,5 +100,10 @@ choices.forEach(choice => {
 
     });
 });
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
 
 startGame();
